44cc2733c0ea529c5885ca3e126d727786dbd900
Vibhutha Kumarage <vibhutha.kumarage@dialog.lk>
2022-08-11 12:39:40 +0530

    /transaction POST controller modified with the transactionService

--- src/main/java/com/example/natwestcasestudy/rest/TransactionController.java
+++ src/main/java/com/example/natwestcasestudy/rest/TransactionController.java
@@ -3,9 +3,12 @@ package com.example.natwestcasestudy.rest;
 import com.example.natwestcasestudy.entity.Transaction;
 import com.example.natwestcasestudy.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,4 +23,17 @@ public class TransactionController {
         return transactionService.getAllTransactions();
     }
     
+    @RequestMapping(method = RequestMethod.POST, value = "/transaction")
+    public String postTransaction(@RequestBody Transaction transaction) {
+        int response = transactionService.makeTransaction(transaction);
+        if (response == 1) {
+            // Account not found
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Account Not Found");
+        } else if (response == 2) {
+            // Account balance is not sufficient
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Insufficient Account Balance");
+        } else {
+            return "OK";
+        }
+    }
 }
--- src/main/java/com/example/natwestcasestudy/service/TransactionService.java
+++ src/main/java/com/example/natwestcasestudy/service/TransactionService.java
@@ -2,6 +2,7 @@ package com.example.natwestcasestudy.service;
 
 import com.example.natwestcasestudy.data.DataSource;
 import com.example.natwestcasestudy.entity.Transaction;
+import com.example.natwestcasestudy.service.utility.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +13,30 @@ public class TransactionService {
     
     @Autowired
     DataSource dataSource;
+    @Autowired
+    Validator validator;
     
     public List<Transaction> getAllTransactions() {
         return dataSource.transactionsList;
     }
     
+    public int makeTransaction(Transaction transaction) {
+        // Adding transaction to the database as a pending transaction
+        dataSource.transactionsList.add(transaction);
+        
+        // Needs to validate accountId is exists on the database
+        if (validator.validateAccounts(transaction)) {
+            // Check the amount is available on the source account
+            if (validator.validateAmount(transaction)) {
+                // Do the transaction
+                return 0;
+            } else {
+                // Source account balance is insufficient
+                return 2;
+            }
+        } else {
+            // Account id is not available
+            return 1;
+        }
+    }
 }
