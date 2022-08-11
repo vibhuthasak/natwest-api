96340baa0c612b56f3aff420a1a089011eb6a6d0
Vibhutha Kumarage <vibhutha.kumarage@dialog.lk>
2022-08-11 11:29:16 +0530

    Initial commit

---
+++ src/main/java/com/example/natwestcasestudy/NatwestCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.natwestcasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatwestCaseStudyApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(NatwestCaseStudyApplication.class, args);
+    }
+    
+}
