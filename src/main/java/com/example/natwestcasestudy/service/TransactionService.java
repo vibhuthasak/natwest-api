1d696c1b256bcec153d07c1a2cb8b7eb7063e894
Vibhutha Kumarage <vibhutha.kumarage@dialog.lk>
2022-08-11 13:31:54 +0530

    Added transaction modifier to the TransactionService

--- src/main/java/com/example/natwestcasestudy/service/TransactionService.java
+++ src/main/java/com/example/natwestcasestudy/service/TransactionService.java
@@ -2,6 +2,7 @@ package com.example.natwestcasestudy.service;
 
 import com.example.natwestcasestudy.data.DataSource;
 import com.example.natwestcasestudy.entity.Transaction;
+import com.example.natwestcasestudy.entity.TransactionStatus;
 import com.example.natwestcasestudy.service.utility.EntityModifier;
 import com.example.natwestcasestudy.service.utility.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +32,19 @@ public class TransactionService {
             // Check the amount is available on the source account
             if (validator.validateAmount(transaction)) {
                 entityModifier.transferTransaction(transaction);
+                // Set the transaction to SUCCESS
+                entityModifier.modifyTransaction(transaction, TransactionStatus.SUCCESS);
                 return 0;
             } else {
                 // Source account balance is insufficient
+                // Set the transaction to FAILED
+                entityModifier.modifyTransaction(transaction, TransactionStatus.FAILED);
                 return 2;
             }
         } else {
             // Account id is not available
+            // Set the transaction to FAILED
+            entityModifier.modifyTransaction(transaction, TransactionStatus.FAILED);
             return 1;
         }
     }
