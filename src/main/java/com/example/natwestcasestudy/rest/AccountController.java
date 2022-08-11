ab6b99c974fc62c9d8fb58ce3a01c520c4e6e91e
Vibhutha Kumarage <vibhutha.kumarage@dialog.lk>
2022-08-11 11:46:12 +0530

    Modified the controller to use services

--- src/main/java/com/example/natwestcasestudy/rest/AccountController.java
+++ src/main/java/com/example/natwestcasestudy/rest/AccountController.java
@@ -1,18 +1,22 @@
 package com.example.natwestcasestudy.rest;
 
 import com.example.natwestcasestudy.entity.Account;
+import com.example.natwestcasestudy.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class AccountController {
     
+    @Autowired
+    AccountService accountService;
+    
     @RequestMapping(method = RequestMethod.GET, value = "/accounts")
     public List<Account> getAllAccounts() {
-        return new ArrayList<>();
+        return accountService.getAllAccounts();
     }
 }
--- src/main/java/com/example/natwestcasestudy/rest/TransactionController.java
+++ src/main/java/com/example/natwestcasestudy/rest/TransactionController.java
@@ -1,19 +1,23 @@
 package com.example.natwestcasestudy.rest;
 
 import com.example.natwestcasestudy.entity.Transaction;
+import com.example.natwestcasestudy.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TransactionController {
     
+    @Autowired
+    TransactionService transactionService;
+    
     @RequestMapping(method = RequestMethod.GET, value = "/transactions")
     public List<Transaction> getAllTransactions() {
-        return new ArrayList<>();
+        return transactionService.getAllTransactions();
     }
     
 }
